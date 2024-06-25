41ddcd3dd6ada871198fe13bf5241914faecaafb
JianBBB <sbganne@naver.com>
2024-06-26 04:37:08 +0900

    FEAT: DTO (AddArticleRequest) 클래스 생성
    1. 블로그 글 SAVE를 위한 DTO 클래스 생성
    
    Resolves : #1

---
+++ src/main/java/dev/danny/basicblog/dto/AddArticleRequest.java
@@ -0,0 +1,21 @@
+package dev.danny.basicblog.dto;
+
+import dev.danny.basicblog.domain.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+    private String title;
+    private String content;
+
+    public Article toEntity(){
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
