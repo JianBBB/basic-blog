c42dfde94636fcfe4a8e7ab8e0b334144d98edcd
JianBBB <sbganne@naver.com>
2024-06-29 00:22:04 +0900

    FEAT: API Controller 레이어에 findAll() 메서드 최초 구현
    1. BlogApiController 클래스에 findAllArticles 메서드 추가
    2. Article -> DTO 변환 후 응답값 반환
    
    Resolves : #5

--- src/main/java/dev/danny/basicblog/controller/BlogApiController.java
+++ src/main/java/dev/danny/basicblog/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package dev.danny.basicblog.controller;
 
 import dev.danny.basicblog.domain.Article;
 import dev.danny.basicblog.dto.AddArticleRequest;
+import dev.danny.basicblog.dto.ArticleResponse;
 import dev.danny.basicblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +25,17 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    //블로그 글 목록 조회하는 API 메서드
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
