46d31d98cc03564544fe41e9343abc7911090d11
JianBBB <sbganne@naver.com>
2024-07-02 00:22:57 +0900

    FEAT: Service 레이어에 update API 기능 구현
    1.BlogService 파일에 update 메서드 추가
    
    Resolves : #10

--- src/main/java/dev/danny/basicblog/service/BlogService.java
+++ src/main/java/dev/danny/basicblog/service/BlogService.java
@@ -2,7 +2,9 @@ package dev.danny.basicblog.service;
 
 import dev.danny.basicblog.domain.Article;
 import dev.danny.basicblog.dto.AddArticleRequest;
+import dev.danny.basicblog.dto.UpdateArticleRequest;
 import dev.danny.basicblog.repository.BlogRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -32,5 +34,16 @@ public class BlogService {
     //블로그 글 삭제 메서드
     public void delete(Long id){
         blogRepository.deleteById(id);
+    }
+
+    //블로그 글 수정 메서드
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("not found :" + id));
+
+        article.update(request.getTitle(),request.getContent());
+
+        return article;
     }
 }
