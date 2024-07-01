2992cdd776d3b6533bc85a013d2961cb6647590a
JianBBB <sbganne@naver.com>
2024-07-02 00:38:59 +0900

    FEAT: Update API 기능을 위한 DTO에 @setter 추가
    1. 테스트를 위해서 DTO 생성이 필요하므로 @setter 추가
    
    Resolves : #10

--- src/main/java/dev/danny/basicblog/dto/UpdateArticleRequest.java
+++ src/main/java/dev/danny/basicblog/dto/UpdateArticleRequest.java
@@ -1,12 +1,16 @@
 package dev.danny.basicblog.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class UpdateArticleRequest {
     private String title;
     private String content;
+
 
 }
