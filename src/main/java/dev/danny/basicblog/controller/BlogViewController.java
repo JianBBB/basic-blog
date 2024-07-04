bc11ee60d75dc3c41c9c4ff6c3246c27f4002883
JianBBB <sbganne@naver.com>
2024-07-05 04:44:11 +0900

    FEAT: Controller에 블로그 글 수정, 생성을 위한 메서드 추가
    1. BlogViewController의 newArticle 메서드 추가
     - 파라미터로 받은 id가 null이면 등록 그 외엔 수정
    
    Resolves : #12

--- src/main/java/dev/danny/basicblog/controller/BlogViewController.java
+++ src/main/java/dev/danny/basicblog/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class BlogViewController {
 
     private final BlogService blogService;
 
+    //블로그 글 목록 조회를 위한 메서드
     @GetMapping("/articles")
     public String getArticles(Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll().stream()
@@ -29,11 +31,25 @@ public class BlogViewController {
         return "articleList";
     }
 
+    //블로그 글 조회를 위한 메서드
     @GetMapping("/articles/{id}")
     public String getArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article",new ArticleViewResponse(article));
         return "article";
+    }
+
+    //블로그 글 수정, 생성을 위한 메서드
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model){
+        if(id == null){
+            model.addAttribute("article",new ArticleViewResponse());
+        }else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 
 }
