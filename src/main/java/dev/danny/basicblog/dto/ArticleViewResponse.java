5e392100af74ce70f18679faf775cf21d031d952
JianBBB <sbganne@naver.com>
2024-07-05 04:24:11 +0900

    FEAT: 블로그 글 조회를 위한 응답 DTO 생성
    1. ArticleViewResponse DTO 생성
    
    Resolves : #12

---
+++ src/main/java/dev/danny/basicblog/dto/ArticleViewResponse.java
@@ -0,0 +1,25 @@
+package dev.danny.basicblog.dto;
+
+import dev.danny.basicblog.domain.Article;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+
+}
