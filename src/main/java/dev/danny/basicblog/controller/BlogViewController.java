d4b741b9d864f431da50cd5e5a33676dcd2dd556
JianBBB <sbganne@naver.com>
2024-07-05 04:26:24 +0900

    FEAT: Controller 레이어에 블로그 글 조회를 위한 메서드 추가
    1. getArticle 메서드 추가
    
    Resolves : #12

--- src/main/java/dev/danny/basicblog/controller/BlogViewController.java
+++ src/main/java/dev/danny/basicblog/controller/BlogViewController.java
@@ -2,11 +2,13 @@ package dev.danny.basicblog.controller;
 
 import dev.danny.basicblog.domain.Article;
 import dev.danny.basicblog.dto.ArticleListViewResponse;
+import dev.danny.basicblog.dto.ArticleViewResponse;
 import dev.danny.basicblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,6 +27,13 @@ public class BlogViewController {
         model.addAttribute("articles",articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article",new ArticleViewResponse(article));
+        return "article";
     }
 
 }
